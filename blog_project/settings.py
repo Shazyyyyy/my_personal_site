d281db0d3824c97840a66f3397890639f1f4afcd
Shahzoda <korvaloool1@gmail.com>
2021-05-31 18:21:02 +0500

    many fixes

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,7 +134,7 @@ EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
 STATICFILES_DIRS = (
-    os.psth.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 
